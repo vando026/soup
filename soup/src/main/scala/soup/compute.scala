851666ba8c34546e1ed502f02434192e4f078701
Alain_Vandormael <avandormael@conviva.com>
2023-04-02 12:27:44 -0700

    Clean up code

--- soup/src/main/scala/soup/compute.scala
+++ soup/src/main/scala/soup/compute.scala
@@ -13,6 +13,7 @@ object compute {
     val strata: Column
     val df: Double
     val vprod: Column
+    def smpMVariance: Column
 
     /** Calculate the finite population correction factor: 1 - (n/N) */
     def fpc_(n: Column, N: Column): Column = lit(1) - (n / N)
@@ -35,16 +36,8 @@ object compute {
         .withColumn("fpc", fpc_(col("n"), col("N_")))
     }
 
-    /** Calculate the sampling mean and variance. `yprod` is a product or
-     *  multiplier to calculate the sample mean or total, which is
-     *  n/N or N, respectively. `vprod` is  the sample mean or total variance 
-     *  multiplier, which is (n/N)^2 or N^2, respectively. */
-    // def getEst(data: DataFrame, N: Double, yprod: Column, vprod: Column): 
-    //     DataFrame = {
-    //   data.select(
-    //       (col("ybar") * yprod).alias("yest"),
-    //       (col("fpc") * vprod * (col("yvar") / col("n"))).alias("yvar"))
-
+    /** Get the final estimates from sampled data with standard error and
+     *  confidence intervals. */ 
     def getEst(data: DataFrame, df: Double, alpha: Double): DataFrame = {
       val tstat = new TDistribution(df)
         .inverseCumulativeProbability(1 - (alpha/2))
@@ -59,26 +52,9 @@ object compute {
        .withColumn("ub", col("yest") + width)
     }
 
-    // def getMean(N: Double): Column = (col("ybar") * col("N_") / N).alias("yest")
-    // def getTotal(): Column = (col("ybar") * col("N_")).alias("yest")
-    def smpMVariance: Column
-    /** Calculate the confidence intervals. 
-     *  @param alpha The default value is 0.05 for 95% conidence intervals. 
-    */
-    // case class confint_(data: DataFrame, df: Double, alpha: Double) {
-    //   val tstat = new TDistribution(df)
-    //     .inverseCumulativeProbability(1 - (alpha/2))
-    //   val width = lit(tstat) * col("yse")
-    //   def lb(): Column = col("yest") - width 
-    //   def ub(): Column = col("yest") + width
-    // }
     /** Calculate the coefficient of variation. */
     def cv_(yest: Column, yse: Column): Column = yse / yest
   
-  // }
-
-  // trait Compute extends Survey {
-    
     /** Dataframe of summary statistics. For SRS, strata = 1. */
     val sdat = preCompute(data)
 
@@ -86,49 +62,15 @@ object compute {
     def summary(): DataFrame = sdat 
 
     /** Return the population size. */
-    def N(): Double = {
+    def N(): Double = 
       sdat.agg(sum("N_")).collect()(0).getDouble(0)
-    }
 
     /** Return the sample size. */
-    def n(): Double = {
+    def n(): Double =
       sdat.agg(sum("n")).collect()(0).getDouble(0)
-    }
 
     /** Return the number of strata. */
     def nstrata(): Long =  sdat.count
-
-    /** Calculate the survey mean, with standerd error and
-     *  confidence intervals.
-     *  @param alpha The default value is 0.05 for 95% conidence intervals. 
-     *  */
-
-    // def svymean(alpha: Double = 0.05): DataFrame = {
-    //   val mdat = getEst(sdat, N(), 
-    //     yprod =  (col("N_")  / N), vprod) 
-    //   val ci = confint_(mdat, df, alpha)
-    //   mdat.withColumn("lb", ci.lb())
-    //     .withColumn("ub", ci.ub())
-    // }
-
-    /** Calculate the survey total, with standerd error and
-     *  confidence intervals.
-     *  @param alpha The default value is 0.05 for 95% conidence intervals. 
-     *  */
-
-    // def svytotal(alpha: Double = 0.05): DataFrame = {
-    //   val tdat = getEst(sdat, N(), 
-    //     yprod = col("N_"), vprod = pow(col("N_"), 2))
-    //   val ci = confint_(tdat, df, alpha)
-    //   tdat
-    //     .withColumn("lb", ci.lb())
-    //     .withColumn("ub", ci.ub())
-    // }
-
-    // /** Calculate the confidence intervals. */
-    // def cv(): DataFrame = {
-    //   svymean().select(col("yest") / col("yse").alias("cv"))
-    // }
 
   }
 
@@ -136,9 +78,11 @@ object compute {
     def smpMean(N: Double): Column = (col("ybar") * col("N_") / N).alias("yest")
     def smpMVariance: Column
     val mdat = sdat.select(smpMean(N()), smpMVariance)
-    def svymean(alpha: Double = 0.05): DataFrame = {
-      getEst(mdat, df, alpha)
-    }
+    /** Calculate the survey mean, with standerd error and
+     *  confidence intervals.
+     *  @param alpha The default value is 0.05 for 95% conidence intervals. 
+     *  */
+    def svymean(alpha: Double = 0.05): DataFrame = getEst(mdat, df, alpha)
   }
 
   trait SVYTotal extends Survey {
@@ -146,11 +90,12 @@ object compute {
     def smpTVariance(): Column = 
       (col("fpc") * pow(col("N_"), 2) * (col("yvar") / col("n"))).alias("yvar")
     def tdat = sdat.select(smpTotal, smpTVariance)
-    def svytotal(alpha: Double = 0.05): DataFrame = {
-      getEst(tdat, df, alpha)
-    }
+    /** Calculate the survey mean, with standerd error and
+     *  confidence intervals.
+     *  @param alpha The default value is 0.05 for 95% conidence intervals. 
+     *  */
+    def svytotal(alpha: Double = 0.05): DataFrame =  getEst(tdat, df, alpha)
   }
-
 
 }
 
