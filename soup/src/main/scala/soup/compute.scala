c85829946fda0dac4882452b13f32205b0d53f8b
Alain_Vandormael <avandormael@conviva.com>
2023-09-22 13:11:43 -0700

    Release new version

--- soup/src/main/scala/soup/compute.scala
+++ soup/src/main/scala/soup/compute.scala
@@ -1,65 +1,47 @@
-package conviva.soup
+package conviva.soup 
 
-/* TODO: Add N parameter to Summarize
- *  return N as value
- *  return data
- * Have different Summarize for Cluster
- * Ratio estimation
-*/
-
-object Compute {
+object Stats {
 
   import org.apache.commons.math3.distribution.TDistribution
   import org.apache.spark.sql.{SparkSession, DataFrame, Column}
   import org.apache.spark.sql.functions._
 
-  /** Prepare the data for estimation by taking mean and variance of y with
-   *  sample size smpN and population size N. 
-   *  @param data The input dataframe. 
-   *  @param y The column name of the quantity or variable to estimate.
-   *  @param N The column name for the population size.
-   *  @param strata  The name of the column to stratify or group the estimates by.
-   *  */
-  case class Summarize(
-      dat: DataFrame, y: String, N: String = "N", strata: String = "") {
-    def compute(): DataFrame = {
-      assert(dat.columns.contains("N"), "Column called N required for population total.")
-      val byVar = strata match {
-        case x if x == "" => lit(1)
-        case _ => col(strata)
-      }
-      dat.groupBy(byVar).agg(
-        mean(col(y)).alias("ybar"),
-        sum(col(y)).cast("double").alias("ysum"),
-        variance(col(y)).alias("yvar"),
-        count(lit(1)).cast("double").alias("smpN"),
-        first(col("N")).alias("N")
-      )
-      .withColumn("fpc", lit(1) - (col("smpN") / col("N")))
-      .withColumn("weight", col("N") / col("smpN"))
-    }
-  }
-
-  trait Survey {
-
-    /** Get the final estimates from sampled data with standard error and
-     *  confidence intervals.
-     *  @param data A dataset processed from `Summarized`.
-     *  */ 
-    val data: DataFrame
+  trait  Compute {
+    val dat: DataFrame
+    val popSize: Column
+    val weights: Column
+    val strata: Column
+    val alpha: Double
+    //
     def tstat(alpha: Double) = udf((col: Double) => {
       new TDistribution(col).inverseCumulativeProbability(1 - (alpha / 2))
     })
-    def calcDf(): Column = col("smpN") - lit(1)
-    def smpMean(): Column = ((col("ysum") * col("weight")) / col("N")).alias("yest")
-    def smpMVar(): Column = {
-      (col("fpc") * (col("yvar") / col("smpN"))).alias("yvarFpc")
+    val strata_ = Option(strata).getOrElse(lit(1))
+    val weight_ = Option(weights).getOrElse(col("popSize") / col("smpSize"))
+    val fpc = lit(1) - (col("smpSize") / col("popSize"))
+    //
+    def summary(y: Column): DataFrame = {
+      dat
+        .withColumn("strata", strata_)
+        .groupBy("strata").agg(
+          mean(y).alias("ybar"),
+          sum(y).cast("double").alias("ysum"),
+          variance(y).alias("yvar"),
+          count(lit(1)).cast("double").alias("smpSize"),
+          first(popSize).alias("popSize")
+        )
+      .withColumn("fpc", fpc)
+      .withColumn("weight", weight_)
     }
+    //
+    def calcDf(): Column = col("smpSize") - lit(1)
+    def smpMean(): Column = ((col("ysum") * col("weight")) / col("popSize")).alias("yest")
+    def smpMVar(): Column = (col("fpc") * (col("yvar") / col("smpSize"))).alias("yvarFpc")
     def smpTotal(): Column = (col("ysum") * col("weight")).alias("yest")
     def smpTVar(): Column = 
-      (col("fpc") * pow(col("N"), 2) * (col("yvar") / col("smpN"))).alias("yvarFpc")
+        (col("fpc") * pow(col("popSize"), 2) * (col("yvar") / col("smpSize"))).alias("yvarFpc")
 
-    def getEst(data: DataFrame, alpha: Double): DataFrame = {
+    def getEst(data: DataFrame): DataFrame = {
        data
         .withColumn("yse",  sqrt(col("yvarFpc")))
         .withColumn("df", calcDf)
@@ -67,7 +49,7 @@ object Compute {
         .withColumn("width", col("tstat") * col("yse"))
         .withColumn("lb", col("yest") - col("width"))
         .withColumn("ub", col("yest") + col("width"))
-        .select(data.columns(0), "yest", "yse", "lb", "ub", "df", "tstat")
+        .select(data.columns(0), "yest", "yse", "lb", "ub", "df", "tstat", "smpSize", "popSize")
     }
   }
 
