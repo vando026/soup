f29f6eb80f31990b4d0e7f81c9d0c570584ee02e
Alain_Vandormael <avandormael@conviva.com>
2023-09-22 13:47:44 -0700

    Update docs

--- README.md
+++ README.md
@@ -4,6 +4,6 @@
 
 <h1 align="center"> conviva-soup</h1>
 
-A Scala/Spark library with tools to compute statistics from sampled Conviva Session Summary data. The current implementation includes computations for the simple random sampling, stratified random sampling, and cluster sampling designs. Please see the 
-Demo Page (https://github.com/Conviva-Internal/soup/wiki) for more info. 
+A Scala library to estimate totals, means, and ratios from simple random sample or stratified random sample designs. Please see the Demo Page (https://github.com/Conviva-Internal/soup/wiki) for more info. 
  
+"For large populations it is the size of the sample taken, not the percentage of the population sampled, that determines the precision of the estimator: If your soup is well stirred, you need to taste only one or two spoonfuls to check the seasoning, whether you have made 1 liter or 20 liters of soup." Sharon Lohr - Sampling Design and Analysis (2022).
--- soup/src/test/scala/soup/SRS_Offline.scala
+++ soup/src/test/scala/soup/SRS_Offline.scala
@@ -1,11 +1,10 @@
 package conviva.soup
 
-class SRSDesignSuite extends munit.FunSuite {
+class SRSDesignSuite2 extends munit.FunSuite {
 
   import org.apache.spark.sql.{SparkSession, DataFrame, Column}
   import org.apache.spark.sql.functions._
-  import conviva.soup.Design.{Simple}
-  import conviva.soup.Compute._
+  import conviva.soup.Design._
 
   val spark = SparkSession
     .builder()
@@ -14,17 +13,14 @@ class SRSDesignSuite extends munit.FunSuite {
 
     val path = "./src/test/data"
 
-    val srs_ = spark.read
+    val agsrs = spark.read
       .option("inferSchema", "true")
       .option("header", "true")
-      .csv(s"$path/agsrs.csv")
-      .select(
-        col("acres92"),
-        when(col("acres92") < 2e5, 1).otherwise(0).alias("lt200k")
-      )
+      .csv(s"$path/agsrs2.csv")
+      .withColumn("lt200k", when(col("acres92") < 2e5, 1).otherwise(0))
       .withColumn("N", lit(3078.0))
 
-    val strdat_ = spark.read
+    val agstrat = spark.read
       .option("inferSchema", "true")
       .option("header", "true")
       .csv(s"$path/agstrat.csv")
@@ -35,20 +31,24 @@ class SRSDesignSuite extends munit.FunSuite {
         .when(col("region") === "W", 422.0))
 
     test("Agsrs props should be expected") {
-      val srs = Summarize(srs_, y = "lt200k").compute
-      val ltsrs = Simple(srs)
-      val t1 = ltsrs.svymean()
+      val srs = SRS(agsrs, popSize = col("N"))
+      val t1 = srs.svymean(y = col("lt200k"))
       assertEquals(t1.select("yest").first.getDouble(0), 0.51)
       assertEquals(t1.select(round(col("yse"), 4)).first.getDouble(0), 0.0275)
       assertEquals(t1.select(round(col("lb"), 3)).first.getDouble(0), 0.456)
       assertEquals(t1.select(round(col("ub"), 3)).first.getDouble(0), 0.564)
+      val srs1 = SRS(agsrs, popSize = col("N"), weights = lit(3078.0 / 300))
+      val t2 = srs1.svymean(y = col("lt200k"))
+      assertEquals(t2.select("yest").first.getDouble(0), 0.51)
+      assertEquals(t2.select(round(col("yse"), 4)).first.getDouble(0), 0.0275)
+      assertEquals(t2.select(round(col("lb"), 3)).first.getDouble(0), 0.456)
     }
 
     test("Agsrs means and totals should be expected") {
-      val srs = Summarize(srs_, y = "acres92").compute
-      val dsrs = Simple(srs)
-      val t1 = dsrs.svymean()
-      val t2 = dsrs.svytotal()
+      val dsrs = SRS(agsrs, popSize = col("N"))
+      val srs = dsrs.summary(col("acres92"))
+      val t1 = dsrs.svymean(col("acres92"))
+      val t2 = dsrs.svytotal(col("acres92"))
       assertEquals(srs.select(round(col("ybar"))).first.getDouble(0), 297897.0)
       assertEquals(srs.select(round(col("weight"), 3)).first.getDouble(0), 10.26)
       assertEquals(srs.select(round(col("ysum"))).first.getDouble(0), 89369114.0)
@@ -63,31 +63,39 @@ class SRSDesignSuite extends munit.FunSuite {
     }
 
     test("Agsrs means by grouping variable should be expected") {
-      val srs = Summarize(strdat_, y = "acres92", strata = "region").compute
-      val dsrs = Simple(srs)
-      val t1 = dsrs.svymean()
+      val dsrs = SRS(agstrat, popSize = col("N"),  strata = col("region"))
+      val srs = dsrs.summary(col("acres92"))
+      val t1 = dsrs.svymean(col("acres92"))
       // val t0 = dsrs.svytotal()
       assertEquals(
-        srs.where(col("region") === "NE")
+        srs.where(col("strata") === "NE")
         .select(round(col("ybar"), 1)).first.getDouble(0), 97629.8)
       assertEquals(
-        srs.where(col("region") === "NC")
+        srs.where(col("strata") === "NC")
         .select(round(col("ybar"), 1)).first.getDouble(0), 300504.2)
       assertEquals(
-        srs.where(col("region") === "W")
+        srs.where(col("strata") === "W")
         .select(round(col("yvar"))).first.getDouble(0), 396185950266.0)
       assertEquals(
-        t1.where(col("region") === "NE")
+        t1.where(col("strata") === "NE")
         .select(round(col("yest"), 1)).first.getDouble(0), 97629.8)
       assertEquals(
-        t1.where(col("region") === "W")
+        t1.where(col("strata") === "W")
         .select(round(col("yest"), 1)).first.getDouble(0), 662295.5)
       assertEquals(
-        t1.where(col("region") === "NE")
+        t1.where(col("strata") === "NE")
         .select(round(col("yse"), 1)).first.getDouble(0), 18149.5)
       assertEquals(
-        t1.where(col("region") === "S")
+        t1.where(col("strata") === "S")
         .select(round(col("yse"), 1)).first.getDouble(0), 18925.4)
+    }
+
+
+    test("Agsrs svyratio and standard errors should be expected") {
+      val srs = SRS(agsrs, popSize = col("N"))
+      val t1 = srs.svyratio(col("acres92"), col("acres87"))
+      assertEquals(t1.select(round(col("yest"), 6)).first.getDouble(0), 0.986565)
+      assertEquals(t1.select(round(col("yse"), 6)).first.getDouble(0), 0.005750)
     }
 
 }
